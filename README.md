ba4dd425e094d143a425c5a20ca0b34dc051f910
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-09-06 20:06:20 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+![Typing SVG](https://readme-typing-svg.herokuapp.com/?lines=WELCOME+To+VISHAL-1755+REPO!;CREATED+BY+DARK+NEMESIS!;A+USERBOT+SONG+AND+VIDEO+PLAYING+BOT!)</p> 
+
+
+
+
+# Barath
+
+Simple UserBot Based On Pyrogram
+
+
+
+<h4> ᴄʟɪᴄᴋ ᴛʜᴇ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ ᴛᴏ ᴅᴇᴘʟᴏʏ ᴏɴ ʜᴇʀᴏᴋᴜ</h4>    
+<p><a href="https://heroku.com/deploy?template=https://github.com/Vishal-1756/Barath"><img src="https://img.shields.io/badge/Deploy%20To%20Heroku-blueviolet?style=for-the-badge&logo=heroku" width="200""/></a></p>
---
+++ strings.py
@@ -0,0 +1,78 @@
+HELP_CMD = """
+**Module**: [`{module}`]
+**Commands**:[`{help}`]
+"""
+
+
+
+MASS_BAN = """
+#MASS_BAN_EVENT
+
+**From**: [`{chat_id}`] 
+**Total Members**: [`{mem_count}`] 
+**Successfully Banned**: [`{success}`] 
+**Failure to Banned**: [`{failures}`] 
+ """    
+
+
+AFK_STRING = """
+**Currently AFK!**
+
+🤔 **Reason**: `{reason}`
+
+👁️‍🗨️ Last seen: `{seen}`
+📅 Date: `{date}` 
+⏰ Time: `{time}`
+"""
+
+BOT_START = """
+Hello, I am Assistant for **{mention}**
+You can deploy Your Own, To Use Me.
+
+🤖 BOT STATUS: [ {botlive} ]
+🥸 APP STATUS: [ {applive} ]
+"""
+
+RESTART_TEXT1 = """
+[`BOT SYSTEM AWAKEN`]
+
+📅 DATE: [`{date}`]
+⏰ TIME: [`{time}`]
+
+[POWERED BY @Dark_Nemesis0]
+"""
+
+RESTART_TEXT2 = """
+[`USER SYSTEM AWAKEN`]
+
+📅 DATE: [`{date}`]
+⏰ TIME: [`{time}`]
+
+[POWERED BY @Dark_Nemesis0]
+"""
+
+RESTART_TEXT3 = """
+[`MUSIC SYSTEM AWAKEN`]
+
+📅 DATE: [`{date}`]
+⏰ TIME: [`{time}`]
+
+[POWERED BY @Dark_Nemesis0]
+"""
+
+
+PAST_FORMAT = """
+**Paste link**: **[Paste link]({link})**
+**Paste Raw link**: **[Raw View]({raw})**
+**Created at**: {timedate}
+"""
+
+
+BIG_NAME = """
+┏━┓╋┏┓╋╋╋╋╋╋╋┏┳┓
+┃┃┗┓┃┃╋╋╋╋╋╋╋┃┃┃
+┃┏┓┗┛┣━━┳━┓┏━┛┃┗━┳━━┓
+┃┃┗┓┃┃┏┓┃┏┓┫┏┓┃┏┓┃┏┓┃
+┃┃╋┃┃┃┏┓┃┃┃┃┗┛┃┃┃┃┏┓┃
+┗┛╋┗━┻┛┗┻┛┗┻━━┻┛┗┻┛┗┛
+"""
