c6fa10279c90578a060279be6f572488d78dbe0e
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-09-06 20:18:07 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -4,27 +4,6 @@ HELP_CMD = """
 """
 
 
-
-MASS_BAN = """
-#MASS_BAN_EVENT
-
-**From**: [`{chat_id}`] 
-**Total Members**: [`{mem_count}`] 
-**Successfully Banned**: [`{success}`] 
-**Failure to Banned**: [`{failures}`] 
- """    
-
-
-AFK_STRING = """
-**Currently AFK!**
-
-🤔 **Reason**: `{reason}`
-
-👁️‍🗨️ Last seen: `{seen}`
-📅 Date: `{date}` 
-⏰ Time: `{time}`
-"""
-
 BOT_START = """
 Hello, I am Assistant for **{mention}**
 You can deploy Your Own, To Use Me.
@@ -51,16 +30,6 @@ RESTART_TEXT2 = """
 [POWERED BY @Dark_Nemesis0]
 """
 
-RESTART_TEXT3 = """
-[`MUSIC SYSTEM AWAKEN`]
-
-📅 DATE: [`{date}`]
-⏰ TIME: [`{time}`]
-
-[POWERED BY @Dark_Nemesis0]
-"""
-
-
 PAST_FORMAT = """
 **Paste link**: **[Paste link]({link})**
 **Paste Raw link**: **[Raw View]({raw})**
@@ -68,11 +37,4 @@ PAST_FORMAT = """
 """
 
 
-BIG_NAME = """
-┏━┓╋┏┓╋╋╋╋╋╋╋┏┳┓
-┃┃┗┓┃┃╋╋╋╋╋╋╋┃┃┃
-┃┏┓┗┛┣━━┳━┓┏━┛┃┗━┳━━┓
-┃┃┗┓┃┃┏┓┃┏┓┫┏┓┃┏┓┃┏┓┃
-┃┃╋┃┃┃┏┓┃┃┃┃┗┛┃┃┃┃┏┓┃
-┗┛╋┗━┻┛┗┻┛┗┻━━┻┛┗┻┛┗┛
-"""
+
