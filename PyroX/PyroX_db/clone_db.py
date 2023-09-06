32cbd83b3f66dbb188d8e7a0845bd14d569669c2
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-09-06 20:17:14 +0530

    Add files via upload

---
+++ PyroX/PyroX_db/clone_db.py
@@ -0,0 +1,26 @@
+from Barath import DATABASE
+
+db = DATABASE["CLONE"]
+
+
+
+async def store_profile(user_id: int, profile: str, first_name: str, bio: str):
+      filter = {"user_id": user_id}
+      if not db.find_one(filter):
+           string = {"user_id": user_id,"profile": profile,"first_name": first_name, "bio": bio}
+           db.insert_one(string)
+           return 
+      else:
+           update = {"$set": {"profile": profile,"first_name": first_name, "bio": bio}}
+           db.update_one(filter , update)
+           return 
+
+
+async def get_profile(user_id: int):
+     id = {"user_id": user_id}
+     barath = db.find_one(id)
+     if barath:
+          return barath
+     return False
+          
+    
