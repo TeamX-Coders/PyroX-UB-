0bf529d2102025a7d28448dd3df30708dc4b50ce
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-09-07 19:40:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
 
-OWNER_ID = [5690711835, 6312693124]
 HANDLER = ["~", ".","!","?","@","$"]
-BARATH = "https://te.legra.ph/file/6a839cecad51014a32254.mp4"
-GROUP_ID = os.getenv("GROUP_ID", "-1001964645198")
-SOURCE = "https://t.me/Dark_nemesis0"
-LOG_CHAT = -1001964645198
+GROUP_ID = os.getenv("GROUP_ID")
+SOURCE = "https://github.com/TeamX-Coders/PyroX-UB"
+LOG_CHAT = os.getenv("LOG_CHAT")
